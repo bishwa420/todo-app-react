e923267c552b07cc8fe14e74f9c7e11025d7101c
Bishwajit <bishwa420@gmail.com>
2020-11-02 22:08:11 +0600

    variable string added on to do list page

--- to-do-app/src/component/Header.js
+++ to-do-app/src/component/Header.js
@@ -3,8 +3,19 @@ import React from "react"
 import '../style/styles.css'
 
 function Header() {
+
+    const date = new Date()
+    const hours = date.getHours()
+
+    let greetingMessage = ""
+
+    if(hours < 12) greetingMessage = "morning"
+    else if(hours < 18) greetingMessage = "afternoon"
+    else if(hours < 22) greetingMessage = "evening"
+    else greetingMessage = "night"
     return (
         <div>
+            <h1>Good {greetingMessage}!</h1> <br/>
             <h1>TODO list</h1>
         </div>
     )
