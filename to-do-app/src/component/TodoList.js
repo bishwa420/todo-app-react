500d0e6af70f945812c7481cd123e7f4cc27d8a9
Bishwajit <bishwa420@gmail.com>
2020-11-02 21:06:07 +0600

    A plain todo list added (which is hard-coded)

---
+++ to-do-app/src/component/TodoList.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function TodoList() {
+    return (
+        <div>
+            <input type="checkbox" id="swimming" name="swimming" value="swimming"/>
+            <label htmlFor="swimming">Swimming</label>
+            <br/>
+            <input type="checkbox" id="meditation" name="meditation" value="meditation"/>
+            <label htmlFor="meditation">Meditation</label>
+            <br/>
+            <input type="checkbox" id="drinking" name="drinking" value="drinking"/>
+            <label htmlFor="drinking">Drinking</label>
+            <br/>
+        </div>
+    )
+}
+
+export default TodoList
