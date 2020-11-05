a4a0cf4b3cd92781eef3a59cd83fef327bf04456
Bishwajit <bishwa420@gmail.com>
2020-11-05 22:21:36 +0600

    Mapping react component from json data done

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import Joke from "./component/Joke"
 import TodoItem from "./component/TodoItem"
+import JokeData from "./component/JokeData"
 
 function App() {
+  const jokeData = JokeData.map(d => <Joke key = {d.id} question = {d.question} punch = {d.punch}/>)
   return (
       <div className="todo-list">
-          <Joke question="How do you do?" punch="Fine!"/>
+          {jokeData}
           <TodoItem
               id = "swimming"
               name = "swimming"
---
+++ to-do-app/src/component/JokeData.js
@@ -0,0 +1,19 @@
+const JokeData = [
+    {
+        id: 1,
+        "question": "question - a",
+        "punch": "punch - a"
+    },
+    {
+        id: 2,
+        "question": "question - b",
+        "punch" : "punch - b"
+    },
+    {
+        id: 3,
+        "question": "question - c",
+        "punch": "punch - c"
+    }
+]
+
+export default JokeData
