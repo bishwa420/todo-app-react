0e098cb015d66b1accab17eacd1bd4b5e14e2fe8
Bishwajit <bishwa420@gmail.com>
2020-11-15 15:04:03 +0600

    More input fields added inside form component

--- to-do-app/src/component/FormPractice.js
+++ to-do-app/src/component/FormPractice.js
@@ -6,17 +6,21 @@ class FormPractice extends Component{
         super()
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: true,
+            gender: "",
+            favColor: "blue"
         }
         this.handleInputChange = this.handleInputChange.bind(this)
     }
 
     handleInputChange(event) {
 
-        const {name, value} = event.target
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+        const {name, value, type, checked} = event.target
+
+        console.log("checked: " + checked)
+
+        type === "checkbox" ? this.setState({isFriendly: checked})  :this.setState({[name]: value})
     }
 
     render() {
@@ -25,9 +29,54 @@ class FormPractice extends Component{
 
                 <input name={"firstName"} onChange={this.handleInputChange}/>
 
+                <br/>
+
                 <input name={"lastName"} onChange={this.handleInputChange}/>
 
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
+                <br/>
+
+                <label>
+                <input type="checkbox"
+                       checked={this.state.isFriendly}
+                        onChange={this.handleInputChange}/>
+                Is friendly?
+                </label>
+
+                <br/>
+                <label>
+                    <input
+                        type="radio"
+                        name={"gender"}
+                        value={"male"}
+                        onChange={this.handleInputChange}/>
+                        Male
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name={"gender"}
+                        value={"female"}
+                        onChange={this.handleInputChange}/>Female
+                </label>
+
+                <br/>
+
+                <label>
+                    Favorite color:
+                    <select value={this.state.favColor}
+                            name={"favColor"}
+                            onChange={this.handleInputChange}>
+                        <option value={"blue"}>Blue</option>
+                        <option value={"green"}>Green</option>
+                        <option value={"red"}>Red</option>
+                        <option value={"yellow"}>Yellow</option>
+                    </select>
+                </label>
+
+                <h1>Name: {this.state.firstName} {this.state.lastName}</h1>
+                <br/>
+                <h3>Gender: {this.state.gender}</h3>
+
             </form>
         )
     }
